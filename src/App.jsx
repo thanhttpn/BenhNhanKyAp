161a2b4ceef3a95e92dd22ef0c17f34ba9153a7d
thanhttpn <thanhttpn@hotmail.com>
2025-11-26 07:48:50 +0700

    test đăng nhập

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import LoginPage from './components/LoginPage';
 import UserProfile from './components/UserProfile';
 import './App.css';
-
+import { useAuth } from './context/AuthContext'; // <-- SỬ DỤNG CUSTOM HOOK
 function App() {
   // Lấy token từ localStorage
-  const token = localStorage.getItem('authToken');
+  const { user } = useAuth(); // <-- Lấy trạng thái user từ context
 
   return (
-    <div className="App">
-      {token ? <UserProfile /> : <LoginPage />}
+    <div className="App">      
+      {user ? <UserProfile /> : <LoginPage />}
     </div>
   );
 }
